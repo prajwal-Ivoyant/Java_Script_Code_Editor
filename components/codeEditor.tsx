b44684f701302d8ab2bb12a31e817314efe0ff84
Prajwal Prakash Kumar <prajwalp@ivoyant.com>
2026-01-21 18:29:24 +0530

    All new update (#10)
    * delete n add modal
    
    * tokyo night css

--- components/codeEditor.tsx
+++ components/codeEditor.tsx
@@ -1,17 +1,40 @@
 import { Editor } from "@monaco-editor/react";
-import "./codeEditor.css";
+import type * as Monaco from "monaco-editor";
 import { useEditor } from "../src/context/EditorContext";
 
 export default function CodeEditor() {
     const {
         activeFile,
         updateFileContent,
+
         runCode,
         output,
         iframeRef,
     } = useEditor();
 
     if (!activeFile) return null;
+
+    const handleEditorWillMount = (monaco: typeof Monaco) => {
+        monaco.editor.defineTheme("tokyo-night", {
+            base: "vs-dark",
+            inherit: true,
+            rules: [
+                { token: "comment", foreground: "565f89" },
+                { token: "string", foreground: "9ece6a" },
+                { token: "keyword", foreground: "7aa2f7" },
+                { token: "number", foreground: "ff9e64" },
+                { token: "type", foreground: "2ac3de" },
+            ],
+            colors: {
+                "editor.background": "#1a1b26",
+                "editor.foreground": "#c0caf5",
+                "editorLineNumber.foreground": "#565f89",
+                "editorCursor.foreground": "#c0caf5",
+                "editor.selectionBackground": "#33467c",
+                "editor.inactiveSelectionBackground": "#292e42",
+            },
+        });
+    };
 
     return (
         <>
@@ -32,8 +55,9 @@ export default function CodeEditor() {
                 height="400px"
                 width="100%"
                 language={activeFile.language}
-                theme="vs-dark"
                 value={activeFile.value}
+                beforeMount={handleEditorWillMount}
+                theme="tokyo-night"
                 onChange={(value) => updateFileContent(value ?? "")}
             />
 
@@ -42,18 +66,9 @@ export default function CodeEditor() {
             {/* JS Output */}
             <pre className="result">{output}</pre>
 
-            {/* HTML/CSS Output */}
-            <iframe
-                ref={iframeRef}
-                title="result-frame"
-                style={{
-                    width: "100%",
-                    height: "300px",
-                    border: "1px solid #444",
-                    marginTop: "10px",
-                    background: "white",
-                }}
-            />
+            {activeFile.language === "html" && (
+                <iframe ref={iframeRef} title="result-frame" />
+            )}
         </>
     );
 }
